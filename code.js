933c61d1d8204cfa8ac368699dd9efc5ea98d167
Katie Burger <kburger14@smpanthers.org>
2014-05-20 11:03:28 -0700

    woohoooooooooooo

--- code.js
+++ code.js
@@ -4,3 +4,18 @@ function changeText1() {
 function changeText4() {
 	$("#cuatro").html("no. aint nobody messin with ma click.");
 }
+function game() {
+	var rando = Math.floor(Math.random() * 11);
+        var a = prompt("guess a number entre diez y uno");
+                while (a != rando) {
+                        if (a < rando) {
+                                var a = prompt("too low, guess higher!");
+                        }
+                        else {
+                                var a = prompt("too high, try a lower guess");
+                        }
+                };
+                if (a == rando) {
+                        alert("winnnerrrrrr")
+                        };
+}
